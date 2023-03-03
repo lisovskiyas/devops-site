5ddbcd28fd40bcc830dfd531f59c3fa1269856ce
LISOVSKIYAS <lisovskiy.as@gmail.com>
2023-03-04 01:15:28 +0500

    script2

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,19 @@ myImage.onclick = function() {
       myImage.setAttribute ('src','images/devopsloop.png');
     }
 }
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'DevOps is cool, ' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'DevOps is cool, ' + storedName;
+}
+myButton.onclick = function() {
+  setUserName();
+}
