24ea0d4f5f36519c9b9efd35f99eb6da9663c63a
LISOVSKIYAS <lisovskiy.as@gmail.com>
2023-03-04 00:26:25 +0500

    script2

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,11 @@
-var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/devopsloop.png') {
+      myImage.setAttribute ('src','images/devops2.png');
+    } else {
+      myImage.setAttribute ('src','images/devopsloop.png');
+    }
+}
+
